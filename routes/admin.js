25f4e257f49998f53a26b7ff19cdb51f97fe6f14
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-11-23 17:03:58 +0530

    login paged token checkes

--- routes/admin.js
+++ routes/admin.js
@@ -23,12 +23,30 @@ const verifyLogin = (req, res, next) => {
   }
 };
 
+const loginCheck = (req, res, next) => {
+  if (req.cookies.adminToken) {
+    jwt.verify(
+      req.cookies.adminToken,
+      process.env.JWT_SECERT,
+      (error, decoded) => {
+        if (error) {
+          next();
+        } else {
+          res.redirect("/");
+        }
+      }
+    );
+  } else {
+    next();
+  }
+};
+
 /* GET home page. */
 router.get("/", verifyLogin, function (req, res, next) {
-  res.render("admin/index", { title: "Dashboard", admin: true });
+  res.render("admin/index", { title: "Dashboard", admin: req.admin });
 });
 
-router.get("/login", async (req, res) => {
+router.get("/login", loginCheck, async (req, res) => {
   res.render("admin/login", { title: "Admin Login" });
 });
 
@@ -37,7 +55,9 @@ router.post("/login", async (req, res) => {
     .doLogin(req.body)
     .then((response) => {
       delete response.password;
-      var token = jwt.sign(response, process.env.JWT_SECERT, { expiresIn: 60 });
+      var token = jwt.sign(response, process.env.JWT_SECERT, {
+        expiresIn: "60d",
+      });
       res.cookie("adminToken", token);
       res.redirect("/");
     })
