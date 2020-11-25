4c3be4d6fecb62e65960ebff4bad8c0717ba196e
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-11-25 17:58:52 +0530

    add patients and add doctors form validation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,3 +15,27 @@ eye.addEventListener("click", () => {
     eyeSlash.style.display = "none";
   }
 });
+
+var username = document.querySelector("input[name=username]");
+var usernameFormat = null;
+username.addEventListener("keyup", () => {
+  username.value = username.value.toLowerCase();
+  var format = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+  // var fillSpace =
+  if (format.test(username.value)) {
+    usernameFormat = false;
+  } else {
+    usernameFormat = true;
+  }
+  if (/\s/.test(username.value)) {
+    username.value = username.value.replace(" ", "_");
+  }
+});
+
+const validate = (event) => {
+  if ((usernameFormat && (male.checked = true)) || (female.checked = true)) {
+    return true;
+  } else {
+    return false;
+  }
+};
