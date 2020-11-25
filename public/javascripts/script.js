b65a294b0657b06bc17f63902c6c5cad6bfcd555
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-11-25 18:22:27 +0530

    form validation error displayed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,12 +20,14 @@ var username = document.querySelector("input[name=username]");
 var usernameFormat = null;
 username.addEventListener("keyup", () => {
   username.value = username.value.toLowerCase();
-  var format = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+  var format = new RegExp(/[@~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
   // var fillSpace =
   if (format.test(username.value)) {
     usernameFormat = false;
+    document.querySelector(".error").style.display = "inline";
   } else {
     usernameFormat = true;
+    document.querySelector(".error").style.display = "none";
   }
   if (/\s/.test(username.value)) {
     username.value = username.value.replace(" ", "_");
