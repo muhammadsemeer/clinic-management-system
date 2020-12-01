d0d3d4f3fe3a49d20a337ffeff1b25aca72cc64f
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-01 21:32:23 +0530

    upload bug fixes

--- routes/admin.js
+++ routes/admin.js
@@ -272,15 +272,14 @@ router.post("/patients/:id", verifyLogin, (req, res) => {
 
 router.post("/doctor/upload/:id", (req, res) => {
   let image = req.files.image;
-  // image.mv("./public/images/doctor/" + req.params.id + ".jpg", (err) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.json({ status: false });
-  //   } else {
-  //     res.json({ status: true });
-  //   }
-  // });
-  res.json({ status: false });
+  image.mv("./public/images/doctor/" + req.params.id + ".jpg", (err) => {
+    if (err) {
+      console.log(err);
+      res.json({ status: false });
+    } else {
+      res.json({ status: true });
+    }
+  });
 });
 
 module.exports = router;
