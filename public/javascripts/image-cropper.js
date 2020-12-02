e29bc481f825525b006896c080cfa01428aa319f
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-02 17:10:00 +0530

    image edit option added on image hover

--- public/javascripts/image-cropper.js
+++ public/javascripts/image-cropper.js
@@ -4,14 +4,16 @@ const change = (event) => {
   var files = event.target.files;
   image.src = URL.createObjectURL(files[0]);
 };
-image.addEventListener("load", () => {
-  document.querySelector("#crop-btn").style.display = "flex";
-  cropper = new Cropper(image, {
-    aspectRatio: 1 / 1,
-    viewMode: 2,
-    zoomOnWheel: false,
+if (image) {
+  image.addEventListener("load", () => {
+    document.querySelector("#crop-btn").style.display = "flex";
+    cropper = new Cropper(image, {
+      aspectRatio: 1 / 1,
+      viewMode: 2,
+      zoomOnWheel: false,
+    });
   });
-});
+}
 
 const crop = () => {
   modalup("crop-modal");
