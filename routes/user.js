e9a5fff3f9469c8547a1551db10167480296a277
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-02 21:08:54 +0530

    user-header linked to layout

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("user/index");
+  res.render("user/index", { user: false });
 });
 
 module.exports = router;
