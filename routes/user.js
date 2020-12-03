8d302180ce4127e0c19f3b54b56f5e454de5f266
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-03 20:33:34 +0530

    user page home designed

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("user/index", { user: false });
+  res.render("user/index", {
+    user: true,
+    title: "Galaxieon Care",
+    header: true,
+  });
 });
 
 module.exports = router;
