97d6872e88d589b66336e3d1f7adaa7442b0f911
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-03 21:40:54 +0530

    responsive navbar

--- public/javascripts/media.js
+++ public/javascripts/media.js
@@ -1,12 +1,14 @@
 const menu = document.querySelector(".menu");
 
-menu.addEventListener("click", () => {
-  const sidebar = document.querySelector(".sidebar");
-  const adminDash = document.querySelector(".ml-250");
-  menu.classList.toggle("active");
-  sidebar.classList.toggle("active");
-  adminDash.classList.toggle("active");
-});
+if (menu) {
+  menu.addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    const adminDash = document.querySelector(".ml-250");
+    menu.classList.toggle("active");
+    sidebar.classList.toggle("active");
+    adminDash.classList.toggle("active");
+  });
+}
 var doctorid;
 function modalup(modal, id) {
   doctorid = id;
