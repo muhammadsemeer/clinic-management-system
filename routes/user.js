2b2d0118562057d23705c24b9f513dfbff3758da
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-05 14:25:40 +0530

    user-signup created

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,20 @@
 var express = require("express");
+const adminHelpers = require("../helpers/admin-helpers");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("user/index", {
-    user: true,
-    title: "Galaxieon Care",
-    header: true,
+  adminHelpers.getDoctors().then((response) => {
+    res.render("user/index", {
+      user: true,
+      title: "Galaxieon Care",
+      header: true,
+      doctor: response,
+    });
   });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("user/signup", { title: "Create An Account" });
 });
 
 module.exports = router;
