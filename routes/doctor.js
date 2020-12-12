710b478eac1a7c37acd3438a4a77b82ecb356656
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-12 08:17:37 +0530

    user index page created

--- routes/doctor.js
+++ routes/doctor.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("doctor/index");
+  res.render("doctor/index", {title: "Doctor Dashboard"});
 });
 
 module.exports = router;
