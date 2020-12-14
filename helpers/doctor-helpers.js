03c9d0e410d678bb593ff302cf3551cde8d3b243
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-14 16:42:47 +0530

    getTodays appoitment function writen

--- helpers/doctor-helpers.js
+++ helpers/doctor-helpers.js
@@ -73,4 +73,37 @@ module.exports = {
         });
     });
   },
+  getTodaysAppointment: (doctorId) => {
+    return new Promise(async (resolve, reject) => {
+      let date = new Date().toDateString();
+      console.log(date);
+      let appointment = await db
+        .get()
+        .collection(collection.APPOINTMENT_COLLECTION)
+        .aggregate([
+          {
+            $match: {
+              $and: [
+                { doctor: ObjectId(doctorId) },
+                { status: "Approved" },
+                { date: date },
+              ],
+            },
+          },
+          {
+            $lookup: {
+              from: collection.PATIENT_COLLECTION,
+              localField: "user",
+              foreignField: "_id",
+              as: "user",
+            },
+          },
+          {
+            $unwind: "$user",
+          },
+        ])
+        .toArray();
+      resolve(appointment);
+    });
+  },
 };
