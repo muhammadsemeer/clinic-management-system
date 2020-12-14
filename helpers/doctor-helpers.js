ebfb7f77b1dc7322766cc1d4ed34548647df0129
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-14 14:23:53 +0530

    doctors bookings find helper created

--- helpers/doctor-helpers.js
+++ helpers/doctor-helpers.js
@@ -4,58 +4,6 @@ const { ObjectId } = require("mongodb");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  getTodaysAppointemnts: () => {
-    return new Promise(async (resolve, rejetct) => {
-      let date = new Date().toDateString();
-      let appointment = await db
-        .get()
-        .collection(collection.APPOINTMENT_COLLECTION)
-        .aggregate([
-          {
-            $match: { date: date },
-          },
-          {
-            $lookup: {
-              from: collection.PATIENT_COLLECTION,
-              localField: "user",
-              foreignField: "_id",
-              as: "user",
-            },
-          },
-          {
-            $unwind: "$user",
-          },
-        ])
-        .toArray();
-      resolve(appointment);
-    });
-  },
-  getUpcomingAppointemnts: () => {
-    return new Promise(async (resolve, rejetct) => {
-      let date = new Date().toDateString();
-      let appointment = await db
-        .get()
-        .collection(collection.APPOINTMENT_COLLECTION)
-        .aggregate([
-          {
-            $match: { date: { $ne: date } },
-          },
-          {
-            $lookup: {
-              from: collection.PATIENT_COLLECTION,
-              localField: "user",
-              foreignField: "_id",
-              as: "user",
-            },
-          },
-          {
-            $unwind: "$user",
-          },
-        ])
-        .toArray();
-      resolve(appointment);
-    });
-  },
   dologin: (deatils) => {
     return new Promise(async (resolve, reject) => {
       let emailFound = await db
@@ -79,6 +27,33 @@ module.exports = {
       } else {
         reject({ msg: "Invalid Email or Password" });
       }
+    });
+  },
+  getMybookings: (doctorId) => {
+    return new Promise(async (resolve, reject) => {
+      let bookings = await db
+        .get()
+        .collection(collection.APPOINTMENT_COLLECTION)
+        .aggregate([
+          {
+            $match: {
+              $and: [{ doctor: ObjectId(doctorId) }, { status: "Pending" }],
+            },
+          },
+          {
+            $lookup: {
+              from: collection.PATIENT_COLLECTION,
+              localField: "user",
+              foreignField: "_id",
+              as: "user",
+            },
+          },
+          {
+            $unwind: "$user",
+          },
+        ])
+        .toArray();
+      resolve(bookings);
     });
   },
 };
