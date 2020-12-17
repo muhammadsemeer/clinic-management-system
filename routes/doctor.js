50d619d0c3a1be90606774be4b568b409982d9ad
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-17 16:51:43 +0530

    slot-config route created

--- routes/doctor.js
+++ routes/doctor.js
@@ -244,4 +244,11 @@ router.post("/profile/username/:id", verifyLogin, (req, res) => {
   });
 });
 
+router.get("/profile/slot-config", verifyLogin, (req, res) => {
+  doctorHelpers.getMySlotConfig(req.doctor._id).then((response) => {
+    console.log(response);
+    res.render("doctor/slot-config");
+  });
+});
+
 module.exports = router;
