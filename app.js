3efb7d4bb2f42a9ea992f4dd75532a45c8ccb561
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-12-22 12:38:32 +0530

    disabling x-powered by

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var session = require("express-session");
 var fileUpload = require("express-fileupload");
 require("dotenv").config();
 
+app.disable('x-powered-by')
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
